47efd36621a65d8025160f0634dedd0d07a69e2c
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-04-26 11:26:14 +0100

    DefaultArgs lesson

--- src/main/scala/lectures/part1Basics/DefaultArgs.scala
+++ src/main/scala/lectures/part1Basics/DefaultArgs.scala
@@ -2,4 +2,23 @@ package lectures.part1Basics
 
 object DefaultArgs extends App {
 
+  def trFact(n: Int, acc: Int = 1): Int =
+    if (n <= 1) acc
+    else trFact(n-1, n*acc)
+
+  val fact10 = trFact(10, 2)
+
+
+  def savePicture (format: String, width: Int, height: Int): Unit = println("saving picture...")
+  savePicture("jpg", 800, 600)
+
+  // specify a default value for an argument an the just omitted when calling
+
+  /*
+  1.either pass in every leading argument EG. savePicture("jpg", 800, 600)
+  2. Or name the arguments EG. (width = 800)
+  */
+
 }
+
+
