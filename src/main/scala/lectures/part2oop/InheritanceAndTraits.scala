c5d66f99d6e2662e5dd2019f47b202e88fb7f532
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-04-27 17:00:56 +0100

    Inheritance and Traits

--- src/main/scala/lectures/part2oop/InheritanceAndTraits.scala
+++ src/main/scala/lectures/part2oop/InheritanceAndTraits.scala
@@ -1,9 +1,58 @@
 package lectures.part2oop
 
-object InheritanceAndTraits extends App {
+object Inheritance extends App {
+  // Single class inheritance
+  // default is public
+  // Private - only accessible in its class
+  // protected - only usable in its class and subclasses
+  // single class inheritance
+  sealed class Animal {
+    val creatureType = "wild"
+    def eat = println("nomnom")
+  }
 
-  class Animal
+  class Cat extends Animal {
+    def crunch = {
+      eat
+      println("crunch crunch")
+    }
+  }
+
+  val cat = new Cat
+  cat.crunch
 
 
+  // constructors
+  class Person(name: String, age: Int) {
+    def this(name: String) = this(name, 0)
+  }
+  class Adult(name: String, age: Int, idCard: String) extends Person(name)
 
+  // overriding works for methods aswell as VAL or VAR
+  class Dog(override val creatureType: String) extends Animal {
+    //    override val creatureType = "domestic" // can override in the body or directly in the constructor arguments
+    override def eat = {
+      super.eat
+      println("crunch, crunch")
+    }
+  }
+  val dog = new Dog("K9")
+  dog.eat
+  println(dog.creatureType)
+
+
+  // type substitution (broad: polymorphism)
+  val unknownAnimal: Animal = new Dog("K9")
+  unknownAnimal.eat
+
+  // overRIDING vs overLOADING
+
+  // super is used when you want to reference a method or a field from a parent class
+
+  // preventing overrides
+  // 1 - use final on member
+  // 2 - use final on the entire class
+  // 3 - seal the class = extend classes in THIS FILE, prevent extension in other files
 }
+
+
